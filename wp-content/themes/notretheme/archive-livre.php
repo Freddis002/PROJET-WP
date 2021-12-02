2d70adfe106efd1672dfd65cc8a6219156e3f685
Fred <dislaire.frederic@hotmail.fr>
2021-12-02 18:05:42 +0100

    archive

--- wp-content/themes/notretheme/archive-livre.php
+++ wp-content/themes/notretheme/archive-livre.php
@@ -4,14 +4,19 @@ get_header();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
-		<?php  wp_nav_menu([
+		
+		<?php  
+		wp_nav_menu([
 			'menu' => 'livre-menu',
-		]); ?>
+		]); 
+		?>
 		<?php dynamic_sidebar('before-main-sidebar'); ?>
 		<?php if (have_posts()) : ?>
+			<ul class="livre">
 				<?php while (have_posts()) : the_post(); ?>
                 <?php get_template_part('parts/content', get_post_type()); ?>
 				<?php endwhile; ?>
+				</ul>
 		<?php endif; ?>
 
 	</main><!-- .site-main -->
