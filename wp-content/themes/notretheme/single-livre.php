5dc15227ff8c106a3f3b613776c6002de9821a81
Fred <dislaire.frederic@hotmail.fr>
2021-12-09 19:37:36 +0100

    Test

--- wp-content/themes/notretheme/single-livre.php
+++ wp-content/themes/notretheme/single-livre.php
@@ -5,10 +5,12 @@
 		<?php if ($related_posts && $related_posts->have_posts()) : ?>
 			<h2>Livre</h2>
 			<ul>
+				
 				<?php while ($related_posts->have_posts()) : $related_posts->the_post(); ?>
 					<?php get_template_part('parts/content', 'livres'); ?>
 				<?php endwhile; ?>
 			</ul>
+
 		<?php endif; ?>
 	<?php endif; ?>
 </div><!-- .content-area -->
